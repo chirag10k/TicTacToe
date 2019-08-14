94f9c4722382bb25c261c62dbda27f29012320d8
Chirag Saraogi <gunjansaraogi@Gunjan.local>
2019-08-14 18:03:01 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/home_page.dart';
+
+void main() =>runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: new ThemeData(
+        primaryColor: Colors.black,
+      ),
+      home: new HomePage(),
+    );
+  }
+}
